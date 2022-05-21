8d74b2ce180e74c8aec6be809286c076866c2140
Bibhuti Bhushan Mukhiya <bibhutibhushanmukhiya4086@gmail.com>
2022-05-21 18:02:34 +0545

    searchbook

--- src/components/RoutesPath.js
+++ src/components/RoutesPath.js
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom'
 import Login from './authentication/Login'
 import Register from './authentication/Register'
 import FrontPage from './frontpage/FrontPage'
+import AddBook from './publication/AddBook'
 
 
 
@@ -14,7 +15,8 @@ function RoutesPath() {
             <Route path="/" element={<FrontPage/>} ></Route>
             <Route path="/login" element={<Login/>} ></Route>
             <Route path="/register" element={<Register/>} ></Route>
-            
+            <Route path="/add-books" element={<AddBook/>} ></Route>
+           
         </Routes>
 
 
--- src/components/frontpage/FrontPage.js
+++ src/components/frontpage/FrontPage.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './style.css';
+import logo from '../../statics/logo.png';
+import book from '../../statics/book.png';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+
+
 
 function FrontPage() {
-  return (
-    <div>FrontPage</div>
-  )
+    let navigate = useNavigate();
+    const [keyword, setKeyword] = useState('');
+
+    function onFormSubmit(e){
+        e.preventDefault();
+        if(keyword.length<=3){
+            toast.error('Give some proper hints.')
+
+        }else{
+
+            navigate('/results/' + keyword)
+        }
+    };
+
+      
+
+
+    return (
+       <div className="container-fluid">
+            <div className='frontpage mx-auto my-2 container bg-purple p-2 rounded-3'>
+            <div className='d-flex justify-content-center my-3'>
+                <img className='logoimage ' src={logo} alt="logo" />
+
+            </div>
+            <form >
+                <div className='mx-auto col-lg-5' >
+                    <input type="text" required onChange={(e) => setKeyword(e.target.value)}  className="form-control fs-5 " placeholder="Book hints..." />
+                </div>
+                <div className='d-flex justify-content-center mx-auto my-4' >
+                    <button type="button" onClick={onFormSubmit} className="btn btn-danger col-lg-2 fs-5 ">
+                    <i className="fas fa-search"></i> &nbsp; Search 
+                    </button>
+                </div>
+
+            </form>
+            <div className='d-flex justify-content-center my-2'>
+                <img className='bookimage' src={book} alt="logo" />
+
+            </div>
+
+        </div>
+       </div>
+    )
 }
 
 export default FrontPage
