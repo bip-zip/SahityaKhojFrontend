ce03403d04e78df3a27caa1f04e9c5871aebb6a0
santoshdc1 <dangisantosh456@gmail.com>
2022-06-21 14:41:45 +0545

    :sparkles:UpdateBook

--- src/components/Publication/UpdateBook.js
+++ src/components/Publication/UpdateBook.js
@@ -1,6 +1,23 @@
 import React from 'react'
 
 function UpdateBook() {
+    //   get post
+const getPosts=()=>{
+    axios.get("http://localhost:8080/api/update-book" + feedId).then(result => {
+        if (result.data.status) {
+            setTitle(result.data.data.title)
+            setCategory(result.data.data.category)
+            setContent(result.data.data.content)
+        }
+        else {
+            toast.error("Something went wrong!!")
+        }
+    }).catch(e => {
+        toast.error("Something went wrong!!")
+    })
+}
+  
+  console.log(localStorage.getItem('token'))
   return (
     <div>UpdateBook</div>
   )
