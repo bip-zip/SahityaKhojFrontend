30c1650cb84e9cdc9c66e0cba7bab87647ed644f
santoshdc1 <dangisantosh456@gmail.com>
2022-07-01 22:52:31 +0545

    :sparkles:Book Rating Page

--- src/components/Book/StarRating.js
+++ src/components/Book/StarRating.js
@@ -23,55 +23,10 @@ function StarRating({bookId}) {
 
 
 
-        axios
-            .post("http://localhost:8080/api/ratings/rate", data, config)
-            .then((result) => {
-                console.log(result);
-                if (result.data.success) {
-                    toast.success(result.data.message);
-            
-                } else {
-                    toast.error("Something went wrong");
-                }
-            })
-            .catch((e) => {
-                toast.error("Something went wrong!!");
-            });
-
+ 
     }
 
-    return (
-        <div className="star-rating text-center">
-            {[...Array(5)].map((star, index) => {
-                index += 1;
-                return (
-
-                    <button
-                        title={index + " star"}
-                        type="button"
-                        key={index}
-                        className={index <= (hover || rating) ? "on" : "off"}
-                        //   onClick={() => setRating(index)}
-                        onClick={() => { saveRating(index) }}
-                        onMouseEnter={() => setHover(index)}
-                        onMouseLeave={() => setHover(rating)}
-                        style={{
-                            "backgroundColor": "transparent",
-                            "border": "none",
-                            "outline": "none",
-                            "cursor": "pointer"
-                        }}
-                    >
-                        {/* <span className="star">&#9733;</span> */}
-                        <span class="fa fa-star  fs-5"></span>
-                    </button>
-
-                );
-            })}
-            <p className=' h6 text-secondary'>Rate this book</p>
-
-        </div>
-    );
+ 
 
 
 
