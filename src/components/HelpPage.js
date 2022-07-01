b092b3005d8cc0232848941b00aad721196706fc
santoshdc1 <dangisantosh456@gmail.com>
2022-07-01 22:47:51 +0545

    :sparkles:Help Page

--- src/components/HelpPage.js
+++ src/components/HelpPage.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
+function HelpPage() {
+  return (
+    <>
+      <div className="container-fluid">
+       
+      </div>
+    </>
+  );
+}
 
 export default HelpPage;
